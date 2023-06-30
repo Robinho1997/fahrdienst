593feec223d14b32d6fa95d77b0c29509cdc18d0
Robinho1997 <robin.kempen@gmx.de>
2023-06-30 22:05:50 +0200

    backend index

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const connectDB = require("./db/connect");
+const port = process.env.PORT;
+const cors = require("cors");
+const rides = require("./routes/ride");
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/v1/fahrten", rides);
+
+
+const start = async () => {
+  try {
+    await connectDB(process.env.DB_URL);
+    app.listen(port, console.log(`Server listens on ${port} ...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
