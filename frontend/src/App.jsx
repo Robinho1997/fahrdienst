223756415566330e4c5beb70fabfcbf9e5228836
Robinho1997 <robin.kempen@gmx.de>
2023-07-04 00:54:38 +0200

    Route change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Homepage from "./components/Homepage";
 import Navbar from "./components/Navbar";
+import Landingpage from "./components/LandingPage";
+import Homepage from "./components/Homepage";
 import CreateRide from "./components/CreateRide";
 import AllRides from "./components/AllRides";
+import Chat from "./components/Chat";
 
 function App() {
   return (
     <>
-    <Navbar />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/landing-page" element={<Landingpage />} />
+        <Route path="/chat" element={<Chat />} />
         <Route path="/fahrt-finden" element={<AllRides />} />
         <Route path="/fahrt-erstellen" element={<CreateRide />} />
       </Routes>
