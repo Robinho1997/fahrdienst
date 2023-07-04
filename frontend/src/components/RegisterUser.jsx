5cf4e965ba1e60b6bc0af7fdb92d540e69b8f126
Robinho1997 <robin.kempen@gmx.de>
2023-07-04 21:48:58 +0200

    check if response is ok

--- frontend/src/components/RegisterUser.jsx
+++ frontend/src/components/RegisterUser.jsx
@@ -1,46 +1,62 @@
 import React, { useState } from "react";
 import "../styles/registerUser.css";
-
+import { useNavigate } from "react-router-dom";
 function RegisterUser() {
+  const navigate = useNavigate();
   const [data, setData] = useState({});
 
   const createUser = async (e) => {
     e.preventDefault();
-    await fetch("http://localhost:5000/register", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+
+    try {
+      const response = await fetch("http://localhost:5000/register", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log("Response data:", responseData);
+        navigate("/landing-page");
+      } else {
+        console.error("Response error:", response.status);
+        throw new Error("Request failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
   return (
     <div className="wrapper">
-      <div className="title">Register Form</div>
+      <h2 className="title">Registriere dich jetzt und fahre mit!</h2>
       <form>
         <div className="field">
           <input
             type="text"
+            placeholder="Username"
             onChange={(e) => setData({ ...data, userName: e.target.value })}
           />
-          <label>Username</label>
         </div>
         <div className="field">
           <input
+            placeholder="E-Mail-Adresse"
             onChange={(e) => setData({ ...data, email: e.target.value })}
             type="text"
           />
-          <label>Email Address</label>
         </div>
         <div className="field">
           <input
+            placeholder="Kennwort"
             onChange={(e) => setData({ ...data, password: e.target.value })}
             type="password"
           />
-          <label>Password</label>
         </div>
-        <div className="field">
-          <input onClick={createUser} type="submit" value="Register" />
+        <div className="btn-wrapper">
+          <button className="submit-btn" onClick={createUser} type="submit">
+            Registrieren
+          </button>
         </div>
       </form>
     </div>
